58f783b05d46382330ab030597e01411845ace43
Hemant725744 <bhatthemant268@gmail.com>
2023-09-10 15:29:55 +0530

    Loop

--- CircleDemo.java
+++ CircleDemo.java
@@ -5,11 +5,11 @@ public class CircleDemo
     public static void main(String[] args) 
     {
         float r,area;
-        final float pi=3.14f;
-        Scanner s = new Scanner(System.in);
+        final float pi=3.14f;                   //Set value of pie Constant
+        Scanner s = new Scanner(System.in);     //Accepting the Radius from user
         System.out.print("Enter radius");
         r=s.nextFloat();
-        area = pi*r*r;
+        area = pi*r*r;                          //Calcualting Area
         System.out.print("Area ="+area);
         s.close();
 
